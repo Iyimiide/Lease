49e9d8f4dff7252b9ed177ae394a49f4c9eee570
Iyimiide <124675039+Iyimiide@users.noreply.github.com>
2023-03-24 00:14:38 +0000

    Update registration.php

--- Leasewithease/registration.php
+++ Leasewithease/registration.php
@@ -1,113 +1,77 @@
+<?php
+require 'config.php';
+
+// Redirect user to index page if already logged in
+if(!empty($_SESSION["id"])){
+  header("Location: index.php");
+}
+
+// Handle form submission
+if(isset($_POST["submit"])){
+  $name = $_POST["name"];
+  $username = $_POST["username"];
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  $confirmpassword = $_POST["confirmpassword"];
+
+  // Check if username or email already exists in database
+  $duplicate = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$username' OR email = '$email'");
+  if(mysqli_num_rows($duplicate) > 0){
+    echo "<script> alert('Username or Email is Already Taken'); </script>";
+  }
+  else{
+    if($password == $confirmpassword){
+      // Insert user data into database
+      $query = "INSERT INTO tb_user VALUES('','$name','$username','$email','$password')";
+      mysqli_query($conn, $query);
+      echo "<script> alert('Registration Successful'); </script>";
+    }
+    else{
+      echo "<script> alert('Password Does Not Match'); </script>";
+    }
+  }
+}
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
   <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <title>Create Your Lease With Ease Account</title>
-    <style>
-      body {
-        background-color: #f5f5f5;
-        font-family: Arial, sans-serif;
-      }
-      
-      h2 {
-        text-align: center;
-        margin-top: 50px;
-        font-size: 36px;
-        color: #333;
-      }
-      
-      form {
-        margin: 0 auto;
-        width: 400px;
-        padding: 40px;
-        background-color: #fff;
-        border-radius: 10px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-      }
-      
-      label {
-        display: block;
-        font-size: 16px;
-        color: #333;
-        margin-bottom: 10px;
-      }
-      
-      input[type="text"],
-      input[type="email"],
-      input[type="password"] {
-        display: block;
-        width: 100%;
-        padding: 10px;
-        font-size: 16px;
-        margin-bottom: 20px;
-        border: none;
-        border-radius: 5px;
-        box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-      }
-      
-      button[type="submit"] {
-        display: block;
-        width: 100%;
-        padding: 10px;
-        font-size: 16px;
-        margin-top: 20px;
-        background-color: #333;
-        color: #fff;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-      }
-      
-      button[type="submit"]:hover {
-        background-color: #222;
-      }
-      
-      a {
-        display: block;
-        margin-top: 20px;
-        text-align: center;
-        color: #333;
-        font-size: 16px;
-      }
-      
-      a:hover {
-        color: #222;
-      }
-      
-      .alert {
-        display: block;
-        width: 100%;
-        padding: 10px;
-        background-color: #e74c3c;
-        color: #fff;
-        font-size: 16px;
-        margin-bottom: 20px;
-        border-radius: 5px;
-      }
-    </style>
   </head>
   <body>
-    <h2>Create Your Lease With Ease Account</h2>
-    <form class="" action="" method="post" autocomplete="off">
-      <?php
-      if(isset($_POST["submit"])){
-        $name = $_POST["name"];
-        $username = $_POST["username"];
-        $email = $_POST["email"];
-        $password = $_POST["password"];
-        $confirmpassword = $_POST["confirmpassword"];
-        $duplicate = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$username' OR email = '$email'");
-        if(mysqli_num_rows($duplicate) > 0){
-          echo
-          "<div class='alert'>Username or Email Has Already Taken</div>";
-        }
-        else{
-          if($password == $confirmpassword){
-            $query = "INSERT INTO tb_user VALUES('','$name','$username','$email','$password')";
-            mysqli_query($conn, $query);
-            echo
-            "<div class='alert'>Registration Successful</div>";
-          }
-          else{
-            echo
-            "<div class='alert'>Password Does Not Match</div>";
+    <div class="container">
+      <h2>Create Your Lease With Ease Account</h2>
+      <form action="" method="post" autocomplete="off">
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+        <div class="form-group">
+          <label for="username">Username:</label>
+          <input type="text" class="form-control" id="username" name="username" required>
+        </div>
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+        <div class="form-group">
+          <label for="password">Password:</label>
+          <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+        <div class="form-group">
+          <label for="confirmpassword">Confirm Password:</label>
+          <input type="password" class="form-control" id="confirmpassword" name="confirmpassword" required>
+        </div>
+        <button type="submit" class="btn btn-primary" name="submit">Register</button>
+      </form>
+      <div class="mt-3">
+        Already have an account? <a href="login.php">Login here</a>
+      </div>
+    </div>
+
+    <!-- Bootstrap 5 CDN for styling -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-p1hUMJXFRxG8MeJKtCmGS+IvHyCt/hF8rI93cnHnQ1frAn50XtBA11x8oYZcpxL/" crossorigin="anonymous">
+  </body>
+</html>
+``
